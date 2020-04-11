1fe6675a559c889d53928d44bbb635209f881102
hriteek <hriteekbista@gmail.com>
2020-04-11 11:10:51 +0545

    favourite integration with api

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,30 @@ class Product with ChangeNotifier {
     this.isFavourite = false, // default value is false
   });
 
-  void toogleFavouriteStatus() {
+  // > the http package only throws the error for get and post method, but not for others
+
+  void _setFavValue(bool newValue) {
+    isFavourite = newValue;
+    notifyListeners();
+  }
+
+  void toogleFavouriteStatus() async {
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url =
+        'https://flutter-shop-app-5754f.firebaseio.com/products/$id,json';
+    // this is also potismitic update, here we first store the current state in a variable and change the favourite locally  and then call the api, if api call fails then we roll back the favourite the previous state
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavourite': isFavourite,
+          }));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
