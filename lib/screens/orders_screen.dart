09296010febb2a5d20a0121e6cee0d7b86008acd
hriteek <hriteekbista@gmail.com>
2020-04-11 11:49:32 +0545

    hack for not able to use context in initState

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -14,14 +14,27 @@ class OrdersScreen extends StatefulWidget {
 
 class _OrdersScreenState extends State<OrdersScreen> {
   var _isLoading = false;
+  // @override
+  // void initState() {
+  //   Future.delayed(Duration.zero).then((_) async {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     // with listen:false, you could also make that method call WITHOUT the Future.delayed(...) workaround too!
+  //     await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   });
+  //   super.initState();
+  // }
+
+  // this is just the alternative of the above code
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      // with listen:false, you could also make that method call WITHOUT the Future.delayed(...) workaround too!
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+    _isLoading = true;
+    // with listen:false, you could also make that method call WITHOUT the Future.delayed(...) workaround too!
+    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
       setState(() {
         _isLoading = false;
       });
