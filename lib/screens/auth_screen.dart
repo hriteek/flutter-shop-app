e03b27633f8e76d28e1103765de58a795c236051
hriteek <hriteekbista@gmail.com>
2020-04-14 11:44:03 +0545

    working with animated container widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -112,23 +112,23 @@ class _AuthCardState extends State<AuthCard>
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(
-        milliseconds: 300,
-      ),
-    );
-    _heightAnimation = Tween<Size>(
-      // this class is to define value between
-      begin: Size(double.infinity, 260),
-      end: Size(double.infinity, 320),
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.linearToEaseOut,
-      ),
-    );
-    // _heightAnimation.addListener(() => setState(() {}));
+    // _controller = AnimationController(
+    //   vsync: this,
+    //   duration: Duration(
+    //     milliseconds: 300,
+    //   ),
+    // );
+    // _heightAnimation = Tween<Size>(
+    //   // this class is to define value between
+    //   begin: Size(double.infinity, 260),
+    //   end: Size(double.infinity, 320),
+    // ).animate(
+    //   CurvedAnimation(
+    //     parent: _controller,
+    //     curve: Curves.linearToEaseOut,
+    //   ),
+    // );
+    // // _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -200,7 +200,7 @@ class _AuthCardState extends State<AuthCard>
     } catch (error) {
       print(error);
       const errorMessage =
-          "Could not authenticate you. Please try again later.";
+          'Could not authenticate you. Please try again later.';
       _showErrorDialog(errorMessage);
     }
     setState(() {
@@ -230,18 +230,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-          // height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints: BoxConstraints(
-            minHeight: _heightAnimation.value.height,
-          ),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
+      child: AnimatedContainer(
+        curve: Curves.easeIn,
+        duration: Duration(milliseconds: 300),
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.Signup ? 320 : 260,
         ),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
